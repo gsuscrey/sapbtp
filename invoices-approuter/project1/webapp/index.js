2ba92293ef74d02f93c1a562bc440adac3244f42
gsuscrey <jesus.n2k@gmail.com>
2022-04-23 20:33:07 +0000

    second commit

--- invoices-approuter/project1/webapp/index.js
+++ invoices-approuter/project1/webapp/index.js
@@ -1,13 +1,20 @@
 sap.ui.define([
-    "sap/m/Text"
+//    "sap/m/Text"
+    "sap/ui/core/mvc/XMLView"
 ],/**
  * 
- * @param {typeof sap.m.Text} Text 
+ * @param {typeof sap.ui.core.mvc.XMLView} XMLView 
  */
 
- function (Text) {
-    new Text({
-        text: "Hello World"
-    }).placeAt("content");
+ function (XMLView) {
+    "use strict";
+     XMLView.create({
+         viewName: "GsusInvoices.project1.view.App"
+     }).then(function (oView)
+     { oView.placeAt("content");
+    });
+  //  new Text({
+    //    text: "Hello World"
+   // }).placeAt("content");
  //   alert("UI5 is ready");
 });
